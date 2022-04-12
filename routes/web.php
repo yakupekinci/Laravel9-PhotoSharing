bd3e90707ca38ad199b67c6ded1da8c7d49f10ea
YakupEkinci <99822215+novada35@users.noreply.github.com>
2022-04-12 20:13:06 +0300

    Model and table caegory migrated &Category get and list data from database

--- routes/web.php
+++ routes/web.php
@@ -27,3 +27,10 @@ Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
 
  Route::get('/admin',[AdminHomeController::class,'index'])->name('admin');
 
+
+ //****************************** ADMİN CATEGORY ROUTES ************************************//
+
+ Route::get('/admin/category',[\App\Http\Controllers\AdminPanel\CategoryController::class,'index'])->name('admin_category');
+ Route::get('/admin/category/create',[\App\Http\Controllers\AdminPanel\CategoryController::class,'create'])->name('admin_category_create');
+ Route::post('/admin/category/store',[\App\Http\Controllers\AdminPanel\CategoryController::class,'store'])->name('admin_category_store');
+
