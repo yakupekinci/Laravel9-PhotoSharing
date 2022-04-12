9445cee36024b380533187c3fe7c3ccc1e96ce66
YakupEkinci <99822215+novada35@users.noreply.github.com>
2022-04-12 20:13:06 +0300

    Category Get and List Data from Database

---
+++ resources/views/admin/category/create.blade.php
@@ -0,0 +1,100 @@
+@extends('layouts.adminbase')
+
+@section('title', 'Add Category')
+
+
+
+@section('content')
+
+    <div class="main-panel">
+        <div class="content-wrapper">
+
+            <div class="row">
+                <div class="col-md-12 grid-margin">
+                    <div class="d-flex justify-content-between flex-wrap">
+                        <div class="d-flex align-items-end flex-wrap">
+                            <div class="me-md-3 me-xl-5">
+                                <h2>Add Category</h2>
+                            </div>
+                            <div class="d-flex">
+                                <i class="mdi mdi-home text-muted hover-cursor"></i>
+                                <p class="text-muted mb-0 hover-cursor">&nbsp;/&nbsp;Dashboard&nbsp;/&nbsp;</p>
+                                <p class="text-primary mb-0 hover-cursor">Analytics</p>
+                            </div>
+                        </div>
+                        <div class="d-flex justify-content-between align-items-end flex-wrap">
+                            <button type="button" class="btn btn-light bg-white btn-icon me-3 d-none d-md-block ">
+                                <i class="mdi mdi-download text-muted"></i>
+                            </button>
+                            <button type="button" class="btn btn-light bg-white btn-icon me-3 mt-2 mt-xl-0">
+                                <i class="mdi mdi-clock-outline text-muted"></i>
+                            </button>
+                            <button type="button" class="btn btn-light bg-white btn-icon me-3 mt-2 mt-xl-0">
+                                <i class="mdi mdi-plus text-muted"></i>
+                            </button>
+                            <button class="btn btn-primary mt-2 mt-xl-0">Generate report</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12 grid-margin stretch-card">
+                    <div class="card">
+                        <div class="card-body dashboard-tabs p-0">
+
+
+
+            <div class="row">
+                <div class="card">
+                    <div class="card-body">
+                        <h4 class="card-title">Category</h4>
+
+
+                        <form class="form-inline" action="/admin/category/store"  method="post">
+                            @csrf
+                            <label class= "sr-only" for="inlineFormInputName2">Title</label>
+                            <input type="text" class="form-control mb-2 mr-sm-2" name="title" placeholder="Title">
+
+                            <label class= "sr-only" for="inlineFormInputName2">Keywords</label>
+                            <input type="text" class="form-control mb-2 mr-sm-2" name="keywords" placeholder="Keywords">
+
+                            <label class= "sr-only" for="inlineFormInputName2">Description</label>
+                            <input type="text" class="form-control mb-2 mr-sm-2" name="description" placeholder="Description">
+
+                            <label>Image</label>
+                            <div class="form-group">
+
+
+                                <div class="input-group col-xs-12">
+                                    <input type="file" name="img[]" class="form-control custom-file-input">
+
+                                    <span class="input-group-append">
+                          <button class="file-upload-browse btn btn-primary" type="button">Upload</button>
+                        </span>
+                                </div>
+                            </div>
+
+
+
+
+                            <label >Status</label>
+                            <select  class="form-control mb-2 mr-sm-2"class="form-control mb-2 mr-sm-2" name="status" placeholder="Status" >
+                                    <option>True</option>
+                                    <option>False</option>
+                                   </select>
+
+
+                            <div style="padding-top: 10px">
+                            <button type="submit" class="btn btn-primary mb-2">Save</button>
+                           </div>
+
+                        </form>
+
+                    </div>
+                </div>
+
+
+            </div></div></div></div></div></div>
+
+        <!-- content-wrapper ends -->
+@endsection
---
+++ resources/views/admin/category/index.blade.php
@@ -0,0 +1,107 @@
+@extends('layouts.adminbase')
+
+@section('title', 'Category List')
+
+
+
+@section('content')
+
+    <div class="main-panel">
+        <div class="content-wrapper">
+
+            <div class="row">
+                <div class="col-md-12 grid-margin">
+                    <div class="d-flex justify-content-between flex-wrap">
+                        <div class="d-flex align-items-end flex-wrap">
+                            <div class="me-md-3 me-xl-5">
+                                <h2>Category List</h2>
+                            </div>
+                            <div class="d-flex">
+                                <a href="/admin"> <i class="mdi mdi-home text-muted hover-cursor"></i>
+                                </a>
+                                <p class="text-muted mb-0 hover-cursor">&nbsp;/&nbsp;Dashboard&nbsp;/&nbsp;</p>
+                                <p class="text-primary mb-0 hover-cursor">Analytics</p>
+                            </div>
+                        </div>
+                        <div class="d-flex justify-content-between align-items-end flex-wrap">
+                            <button type="button" class="btn btn-light bg-white btn-icon me-3 d-none d-md-block ">
+                                <i class="mdi mdi-download text-muted"></i>
+                            </button>
+                            <button type="button" class="btn btn-light bg-white btn-icon me-3 mt-2 mt-xl-0">
+                                <i class="mdi mdi-clock-outline text-muted"></i>
+                            </button>
+                            <button type="button" class="btn btn-light bg-white btn-icon me-3 mt-2 mt-xl-0">
+                                <i class="mdi mdi-plus text-muted"></i>
+                            </button>
+                            <button class="btn btn-primary mt-2 mt-xl-0">Generate report</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12 grid-margin stretch-card">
+                    <div class="card">
+                        <div class="card-body dashboard-tabs p-0">
+
+
+
+            <div class="row">
+                <div class="col-md-15 grid-margin stretch-card">
+                    <div class="card">
+                        <div class="card-body">
+                            <h4 class="card-title">Category List</h4>
+
+                            <div class="table-responsive pt-3">
+                                <table class="table table-bordered">
+                                    <thead>
+                                    <tr>
+                                        <th>
+                                            Title
+                                        </th>
+                                        <th>
+                                            Keywords
+                                        </th>
+                                        <th>
+                                            Descirption
+                                        </th>
+                                        <th>
+                                            Image
+                                        </th>
+                                        <th>
+                                            Status
+                                        </th>
+
+                                        <th style="width: 40px">Edit</th>
+                                        <th style="width: 40px">Delete</th>
+                                        <th style="width: 40px">Show</th>
+
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    @foreach($data as $rs )
+                                    <tr>
+                                        <td>{{$rs->id}}</td>
+                                        <td>{{$rs->title}}</td>
+                                        <td>{{$rs->description}}</td>
+                                        <td>{{$rs->image}}</td>
+                                        <td>{{$rs->status}}</td>
+                                        <td><a href="/admin/category/edit{{$rs->id}}"class="btn btn-success"">Edit</a>  </td>
+                                        <td><a href="/admin/category/delete{{$rs->id}}"class="btn btn-primary">Delete</a>  </td>
+                                        <td><a href="/admin/category/show{{$rs->id}}"class="btn btn-warning">Show</a>  </td>
+                                    </tr>
+
+
+                                    @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- content-wrapper ends -->
+@endsection
