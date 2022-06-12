7184a34e47403f6a1e78e99e08e3869fed1b87e5
Yakup Ekinci <99822215+YakupEkinci@users.noreply.github.com>
2022-06-12 14:31:01 +0300

    Laravel check admin user Permission for Admin Panel on Middleware
     Laravel check admin user Permission for Admin Panel on Middleware

--- routes/web.php
+++ routes/web.php
@@ -29,10 +29,10 @@ use App\Http\Controllers\HomeController;
  Route::post('/storemessage', [HomeController::class,'storemessage'])->name('storemessage');
  Route::get('/faq', [HomeController::class,'faq'])->name('faq');
  Route::post('/storecomment', [HomeController::class,'storecomment'])->name('storecomment');
- Route::view('/loginuser','home.login');
- Route::view('/registeruser','home.register');
+ Route::view('/loginuser','home.login')->name('loginuser');
+ Route::view('/registeruser','home.register')->name('registeruser');
  Route::get('/logoutuser',[HomeController::class,'logout'])->name('logoutuser');
-Route::view('/loginadmin','admin.login');
+ Route::view('/loginadmin','admin.login')->name('loginadmin');
  Route::post('/loginadmincheck',[HomeController::class,'loginadmincheck'])->name('loginadmincheck');
 
 
@@ -47,12 +47,11 @@ Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
 
 
 //****************************** ADMİN PANEL ROUTES ************************************//
-Route::prefix('admin/')->name('admin.')->group(function () {
+Route::middleware('admin')->prefix('admin/')->name('admin.')->group(function () {
     Route::get('/', [AdminHomeController::class, 'index'])->name('index');
     //****************************** General Routes ROUTES ************************************//
     Route::get('/setting', [AdminHomeController::class, 'setting'])->name('setting');
     Route::post('/setting/', [AdminHomeController::class, 'settingUpdate'])->name('setting.update');
-
 
 
 
